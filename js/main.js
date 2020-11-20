dd485f7ca7e5da03cc4754805ccdcb41b9b71e28
Gianluca Zanzarella <gianlucazanza@hotmail.com>
2020-11-20 18:40:14 +0100

    inizializzataistanza vue
    inseriti collegamenti delle text input in data. Inseriti dati utenti in data, utili a generare i relativi elementi del dom

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const myApp = new Vue ({
+  el: '#root',
+  data: {
+
+    loggedUser: {
+      name: 'Gianluca',
+      surname: 'Zanzarella',
+      avatar:'img/avatar_io.jpg'
+    },
+
+    contacts: [
+      userOne = {
+        name: 'Michele',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_1.jpg'
+      },
+      userTwo = {
+        name: 'Fabio',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_2.jpg'
+      },
+      userThree = {
+        name: 'Samuele',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_3.jpg'
+      },
+      userFour = {
+        name: 'Luisa',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_4.jpg'
+      },
+      userFour = {
+        name: 'Andrea',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_5.jpg'
+      },
+      userFour = {
+        name: 'Maria',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_6.jpg'
+      },
+      userFour = {
+        name: 'Davide',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_7.jpg'
+      },
+      userFour = {
+        name: 'Giulio',
+        surname: '',
+        lastOnline:'',
+        avatar: 'img/avatar_8.jpg'
+      },
+    ],
+
+    inputMessage: '',
+
+    inputSearchBar: ''
+  }
+})
